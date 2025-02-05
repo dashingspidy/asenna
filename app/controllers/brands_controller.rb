4a0809bfed555585befc7eb3f7b6d7f6c74c0bdf
Arif Hossain <hamid_kawser@hotmail.be>
2025-02-05 23:05:51 +0600

    fixing UI

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -6,25 +6,11 @@ class BrandsController < ApplicationController
   def create
     @brand = Brand.new(brand_params)
     if @brand.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to brands_path, success: "Brand created successfully." }
-      end
+      flash[:success] = "Brand created successfully."
+      redirect_to brands_path
     else
-      respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("brand-form", partial: "brands/form", formats: :html, locals: { brand: @brand }) }
-        format.html { render :index, status: :unprocessable_entity }
-      end
+      render :index, status: :unprocessable_entity
     end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 
   private
