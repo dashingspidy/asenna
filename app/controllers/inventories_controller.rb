8277e48e0deb5ec3757177a38b73e1bc5334be0d
Arif Hossain <hamid_kawser@hotmail.be>
2025-02-11 22:46:37 +0600

    added inventory

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,29 @@
 class InventoriesController < ApplicationController
+  def index
+    @inventories = Inventory.all
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.find_or_initialize_by(product_id: inventory_params[:product_id])
+    if @inventory.new_record?
+      @inventory.assign_attributes(inventory_params)
+    else
+      @inventory.quantity += inventory_params[:quantity].to_i
+    end
+
+    if @inventory.save
+      flash[:success] = "Product Stock added."
+      redirect_to inventories_path
+    else
+      flash[:error] = @inventory.errors.full_messages.join(", ")
+      redirect_to inventories_path
+    end
+  end
+
+  private
+
+  def inventory_params
+    params.expect(inventory: [ :product_id, :quantity, :location ])
+  end
 end
